10fb30900524796ea0c47cedc38d811a49f6356a
Nikolas De Giorgis <bznein@gmail.com>
2019-07-24 22:07:26 +0100

    Update main.cpp

--- Day02/part2/main.cpp
+++ Day02/part2/main.cpp
@@ -7,18 +7,20 @@
 using namespace std;
 
 
+/* Loops s1 ans s2, and check if they differ by exactly one character */
 bool differByOne(std::string s1, std::string s2)
 {
   short totDiff=0;
   for (int i=0; i<s1.size(); ++i)
     {
-      if (s1[i]!=s2[i])
+      if (s1[i]!=s2[i]) /* We have a difference */
         {
-           totDiff+=1;
-           if (totDiff==2)
+           totDiff+=1; /* Update the number of difference */
+           if (totDiff==2) /* We have more than one, return false */
              return false;
         }
     }
+  /* Return true if we have one difference, false otherwise (possible only if we have 0) */
   return totDiff==1;
 }
 
